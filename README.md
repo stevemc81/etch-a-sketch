f31b2c22ba5f00cff1bb374d4fb240241e58c809
Steven McWee <stevenmcwee@gmail.com>
2025-06-02 13:19:17 +0100

    Display a 16x16 grid of boxes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+A browser-based sketchpad/etch-a-sketch kind of thinhg. Created as part of The Odin Project.
---
+++ script.js
@@ -0,0 +1,9 @@
+// Create a 16x16 grid of square divs
+const container = document.querySelector('.container');
+
+for (let i = 0; i < (16 * 16); i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.textContent = i;
+    container.appendChild(square);
+}
